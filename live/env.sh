b8517a6a68f74666aa76a62ed0946e6d00b2783d
Gaston Gonzalez <gaston.gonzalez@yahoo.com>
2019-06-17 08:52:02 -0700

    Added support for PEREGRINE_SITE env var

--- live/env.sh
+++ live/env.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+export PEREGRINE_SITE=themeclean
 export APACHE_DOMAIN=peregrine.local
 export APACHE_PROXY_URL=http://www.peregrine-cms.com/
 export DOCKER_CONTAINER_NAME=apache-live
