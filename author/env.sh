1428e3f45ccbe337e9ac74ffbc71ae0ae7bcd07a
Gaston Gonzalez <gaston.gonzalez@yahoo.com>
2019-06-17 08:57:42 -0700

    Refactored Apache name from 'author' to 'stage'

--- author/env.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export APACHE_DOMAIN=author.peregrine.local
-export APACHE_PROXY_URL=http://www.peregrine-cms.com/
-export DOCKER_CONTAINER_NAME=apache-author
-export DOCKER_IMAGE=peregrinecms/apache-author
---
+++ stage/env.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export APACHE_DOMAIN=stage.peregrine.local
+export APACHE_PROXY_URL=http://www.peregrine-cms.com/
+export DOCKER_CONTAINER_NAME=apache-stage
+export DOCKER_IMAGE=peregrinecms/apache-stage
