44de6480b9cf46616d2a9efaba0d32475d5afffb
Gaston Gonzalez <gaston.gonzalez@yahoo.com>
2019-07-15 07:51:23 -0700

    #124 Added k8s namespace env var to stage to resolve Dockerfile image creation issue.

--- stage/env.sh
+++ stage/env.sh
@@ -2,5 +2,6 @@
 
 export APACHE_DOMAIN=stage.peregrine.local
 export APACHE_PROXY_URL=http://www.peregrine-cms.com/
+export K8S_NAMESPACE=default
 export DOCKER_CONTAINER_NAME=apache-stage
 export DOCKER_IMAGE=peregrinecms/apache-stage
