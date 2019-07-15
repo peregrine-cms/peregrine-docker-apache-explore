1ba2a5ca6b992a1c9c277d07423b44f9f7d52505
Gaston Gonzalez <gaston.gonzalez@yahoo.com>
2019-07-15 07:41:30 -0700

    #124 Added support for k8s namespace env var and enabled access from peregrine pod to pagespeed admin.

--- live/env.sh
+++ live/env.sh
@@ -3,5 +3,6 @@
 export PEREGRINE_SITE=themeclean
 export APACHE_DOMAIN=peregrine.local
 export APACHE_PROXY_URL=http://www.peregrine-cms.com/
+export K8S_NAMESPACE=default
 export DOCKER_CONTAINER_NAME=apache-live
 export DOCKER_IMAGE=peregrinecms/apache-live
