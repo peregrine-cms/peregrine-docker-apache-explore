00173f84f774420c1c3c01e7286bcb9b90ef19e2
Gaston Gonzalez <gaston.gonzalez@yahoo.com>
2020-03-20 13:52:56 -0700

    #322 Added support for mod_auth_openidc

--- stage/env.sh
+++ stage/env.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 export APACHE_DOMAIN=stage.peregrine.local
-export APACHE_PROXY_URL=http://www.peregrine-cms.com/
+export APACHE_PROXY_URL=http://host.docker.internal:8080/
 export K8S_NAMESPACE=default
 export DOCKER_CONTAINER_NAME=apache-stage
 export DOCKER_IMAGE=peregrinecms/apache-stage
+
+export OIDC_PROVIDER_METADATA_URL=PLEASE_SET_ME
+export OIDC_CLIENT_ID=PLEASE_SET_ME
+export OIDC_CLIENT_SECRET=PLEASE_SET_ME
+export OIDC_CRYPTO_PASSPHRASE=PLEASE_SET_ME
