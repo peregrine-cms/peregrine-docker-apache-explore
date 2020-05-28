3b1e94ec09d13990ad81d190c930f1efe9f8518e
Gaston Gonzalez <gaston.gonzalez@yahoo.com>
2020-05-28 08:30:04 -0700

    Removed SSO configuration from apache-stage

--- stage/env.sh
+++ stage/env.sh
@@ -5,11 +5,3 @@ export APACHE_PROXY_URL=http://host.docker.internal:8080/
 export K8S_NAMESPACE=default
 export DOCKER_CONTAINER_NAME=apache-stage
 export DOCKER_IMAGE=peregrinecms/apache-stage:latest
-
-export OIDC_PROVIDER_METADATA_URL=PLEASE_SET_ME
-export OIDC_CLIENT_ID=PLEASE_SET_ME
-export OIDC_CLIENT_SECRET=PLEASE_SET_ME
-export OIDC_CRYPTO_PASSPHRASE=PLEASE_SET_ME
-
-# This should match the shared secret defined in the Sling "Header Authentication Handler Configuration"
-export AUTH_HEADER_SHARED_SECRET=PLEASE_SET_ME
