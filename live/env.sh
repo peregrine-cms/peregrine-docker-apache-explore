3a96ff5259717fcdd3145ad8e4892b9e8a6e7d98
Gaston Gonzalez <gaston.gonzalez@yahoo.com>
2020-05-28 08:03:46 -0700

    Updated apache-level to reflect content structure changes.

--- live/env.sh
+++ live/env.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export PEREGRINE_SITE=themeclean
-export APACHE_DOMAIN=peregrine.local
-export APACHE_PROXY_URL=http://www.peregrine-cms.com/
+export PEREGRINE_SITE=example
+export APACHE_DOMAIN=localhost
+export APACHE_PROXY_URL=http://host.docker.internal:8080/
 export K8S_NAMESPACE=default
 export DOCKER_CONTAINER_NAME=apache-live
 export DOCKER_IMAGE=peregrinecms/apache-live
